9ff766b284b97ff1571cd5e527e91bda9bf119aa
awe20 <145538565+awe20@users.noreply.github.com>
2023-10-06 08:22:50 -0700

    loginn added

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+// import { getToken } from "next-auth/jwt"
+
+
 
 export default NextAuth({
   providers: [
@@ -13,9 +16,26 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  pages: {
-    signIn: '/auth/signin',
-  },
+  // pages: {
+  //   signIn: '/auth/signin',
+  //   signOut: '/auth/signout',
+  //   error: '/auth/error', // Error code passed in query string as ?error=
+  //   verifyRequest: '/auth/verify-request', // (used for check email message)
+  //   newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+  // },
+   
   // NEXTAUTH_SECRET :"atr5-gt65-9jet"
- secret: process.env.NEXTAUTH_SECRET,
+//  secret: process.env.NEXTAUTH_SECRET,
 });
+
+ 
+
+// const secret = process.env.NEXTAUTH_SECRET
+
+// export  async function handler(req:any, res:any) {
+//   // if using `NEXTAUTH_SECRET` env variable, we detect it, and you won't actually need to `secret`
+//   // const token = await getToken({ req })
+//   const token = await getToken({ req, secret })
+//   console.log("JSON Web Token", token)
+//   res.end()
+// }
